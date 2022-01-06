6ed6bdef5ebd9c492eadebccda14d39dde59a1d2
newmersedez <trishkk2001@gmail.com>
2022-01-06 17:51:32 +0300

    temp

--- Controller/Controller.py
+++ Controller/Controller.py
@@ -1,3 +1,9 @@
+from PyQt5 import QtWidgets, QtGui
+from Utils.ServerRequest import *
+import json
+from socket import socket, AF_INET, SOCK_STREAM, SHUT_RDWR
+
+
 class Controller:
     def __init__(self, model):
         self._model = model
@@ -7,7 +13,62 @@ class Controller:
         self._view = view
 
     def loginButtonClick(self):
-        pass
+        inputDialog = QtWidgets.QDialog(self._view)
+        inputDialog.setWindowTitle('Авторизация')
+        inputDialog.setFixedSize(300, 200)
+        form = QtWidgets.QFormLayout(inputDialog)
+
+        labelLogin = QtWidgets.QLabel('Логин')
+        labelLogin.setStyleSheet("QLabel { color : white; }")
+        labelLogin.setFont(QtGui.QFont('Arial', 12))
+        textBoxLogin = QtWidgets.QLineEdit()
+        textBoxLogin.setFont(QtGui.QFont('Arial', 14))
+        textBoxLogin.setStyleSheet("QLineEdit { color : #00B6FF; }")
+
+        labelPassword = QtWidgets.QLabel('Пароль')
+        labelPassword.setStyleSheet("QLabel { color : white; }")
+        labelPassword.setFont(QtGui.QFont('Arial', 12))
+        textBoxPassword = QtWidgets.QLineEdit()
+        textBoxPassword.setFont(QtGui.QFont('Arial', 14))
+        textBoxPassword.setStyleSheet("QLineEdit { color : #00B6FF; }")
+
+        form.addRow(labelLogin)
+        form.addRow(textBoxLogin)
+        form.addRow(labelPassword)
+        form.addRow(textBoxPassword)
+
+        buttonBox = QtWidgets.QDialogButtonBox(QtWidgets.QDialogButtonBox.Cancel | QtWidgets.QDialogButtonBox.Ok)
+        buttonBox.setStyleSheet("QPushButton { color : white; }")
+        form.addRow(buttonBox)
+        buttonBox.accepted.connect(inputDialog.accept)
+        buttonBox.rejected.connect(inputDialog.reject)
+
+        ok = inputDialog.exec_()
+
+        if ok:
+            login = textBoxLogin.text()
+            password = textBoxPassword.text()
+
+            # Create server request
+            serverRequest = ServerRequest()
+            serverRequest.RequestName = 'Auth'
+            serverRequest.Args = [login, password]
+            jsonText = json.dumps(serverRequest.__dict__)
+
+            try:
+                # Send request to server
+                s = socket(AF_INET, SOCK_STREAM)
+                s.connect(('localhost', 5430))
+                s.send(str.encode(jsonText))
+
+                # Get request from server
+                bytes_buffer = s.recv(1024)
+                recvText = bytes_buffer.decode('utf-8')
+                s.shutdown(SHUT_RDWR)
+                s.close()
+                print(recvText)
+            except ConnectionError:
+                QtWidgets.QMessageBox.about(self._view, "Ошибка", "Не удается установить соединение с сервером")
 
     def logoutButtonClick(self):
         pass
--- Utils/ServerRequest.py
+++ Utils/ServerRequest.py
@@ -2,7 +2,6 @@ class ServerRequest:
     def __init__(self):
         self.RequestName = None
         self.Args = []
-        self._socket = None
 
     def sendRequestToServer(self, data: str, host: str, port: int):
         pass
